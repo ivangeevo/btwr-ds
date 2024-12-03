7eeb75900584b8d8f065b8d71865db1d78d637e9
Ivan Geevo <dbowgaming@gmail.com>
2024-12-03 21:09:08 +0200

    bump changes

--- src/main/java/org/ivangeevo/btwr_ds/datagen/DS_ItemTagProvider.java
+++ src/main/java/org/ivangeevo/btwr_ds/datagen/DS_ItemTagProvider.java
@@ -2,14 +2,11 @@ package org.ivangeevo.btwr_ds.datagen;
 
 import btwr.btwrsl.tag.BTWRConventionalTags;
 import btwr.core.tag.BTWRTags;
-import com.bwt.blocks.BwtBlocks;
 import com.bwt.items.BwtItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.registry.tag.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -23,6 +20,7 @@ public class DS_ItemTagProvider extends FabricTagProvider.ItemTagProvider
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup)
     {
+        // Conventional tags
         getOrCreateTagBuilder(ConventionalItemTags.STRINGS)
                 .add(BwtItems.hempFiberItem);
 
@@ -30,12 +28,17 @@ public class DS_ItemTagProvider extends FabricTagProvider.ItemTagProvider
                 .add(BwtItems.friedEggItem)
                 .add(BwtItems.poachedEggItem);
 
-        getOrCreateTagBuilder(BTWRTags.Items.TANNED_LEATHERS)
-                .add(BwtItems.tannedLeatherItem);
-
         getOrCreateTagBuilder(BTWRConventionalTags.Items.DO_KNOCKBACK_ITEMS)
                 .add(BwtItems.netheriteMattockItem)
                 .add(BwtItems.netheriteBattleAxeItem);
+
+        getOrCreateTagBuilder(BTWRConventionalTags.Items.AXES_MAKE_PLANKS)
+                .add(BwtItems.netheriteBattleAxeItem);
+
+        // BTWR-SL tags
+        getOrCreateTagBuilder(BTWRTags.Items.TANNED_LEATHERS)
+                .add(BwtItems.tannedLeatherItem);
+
 
     }
 }
