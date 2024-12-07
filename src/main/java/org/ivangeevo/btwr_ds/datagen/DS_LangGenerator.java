8a6f8b145e905222ad00a00dbcec950d1e208458
Ivan Geevo <dbowgaming@gmail.com>
2024-12-07 23:16:05 +0200

    bump

--- src/main/java/org/ivangeevo/btwr_ds/datagen/DS_LangGenerator.java
+++ src/main/java/org/ivangeevo/btwr_ds/datagen/DS_LangGenerator.java
@@ -2,10 +2,7 @@ package org.ivangeevo.btwr_ds.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
-import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.registry.tag.TagKey;
-import net.minecraft.sound.SoundEvent;
 import org.ivangeevo.btwr_ds.item.BTWRDS_Items;
 
 import java.util.concurrent.CompletableFuture;
