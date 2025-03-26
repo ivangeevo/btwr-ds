aa5e9e42d03c7720fe72095d69165d23c3e3381b
Ivan Geevo <dbowgaming@gmail.com>
2025-03-27 00:46:17 +0200

    bump

--- src/main/java/org/ivangeevo/btwr_ds/datagen/DS_BlockTagProvider.java
+++ src/main/java/org/ivangeevo/btwr_ds/datagen/DS_BlockTagProvider.java
@@ -21,7 +21,7 @@ public class DS_BlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .forceAddTag(BTWRConventionalTags.Blocks.FARMLAND_BLOCKS);
 
         getOrCreateTagBuilder(BTWRConventionalTags.Blocks.WOODEN_MISC_BLOCKS)
-                .add(BwtBlocks.hopperBlock)
+                //.add(BwtBlocks.hopperBlock) - not a good idea
                 .add(BwtBlocks.gearBoxBlock);
 
     }
