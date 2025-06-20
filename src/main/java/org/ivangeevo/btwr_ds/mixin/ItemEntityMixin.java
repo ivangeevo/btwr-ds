7e3d01f9d8c85a8f971dac91ca24319b1e460a0c
Ivan Geevo <dbowgaming@gmail.com>
2025-06-20 15:24:35 +0300

    bump changes

---
+++ src/main/java/org/ivangeevo/btwr_ds/mixin/ItemEntityMixin.java
@@ -0,0 +1,27 @@
+package org.ivangeevo.btwr_ds.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ItemEntity.class)
+public abstract class ItemEntityMixin extends Entity
+{
+
+    public ItemEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    // Changes item entity discard (despawn) timer from 5 min(6000ticks) to 20min(24000ticks)
+    @ModifyConstant(method = "tick", constant = @Constant(intValue = 6000))
+    private int modifyDespawnTime(int constant) {
+        return 24000;
+    }
+
+
+
+}
