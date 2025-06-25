53ca86bee10fb9061df9b0cc8efbaf1ddd30bb31
Ivan Geevo <dbowgaming@gmail.com>
2025-06-25 19:59:16 +0300

    bump changes

--- src/main/java/org/ivangeevo/btwr_ds/mixin/PlayerEntityMixin.java
+++ src/main/java/org/ivangeevo/btwr_ds/mixin/PlayerEntityMixin.java
@@ -2,16 +2,22 @@ package org.ivangeevo.btwr_ds.mixin;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.RangedWeaponItem;
 import net.minecraft.util.math.Vec3d;
 import org.ivangeevo.btwr_ds.util.ArmorWeightManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Predicate;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin {
 
+    // Add extra exhaustion to all actions when wearing armor
     @Inject(method = "addExhaustion", at = @At("HEAD"), cancellable = true)
     private void addExhaustionWithArmorWeight(float originalAmount, CallbackInfo ci) {
         PlayerEntity player = (PlayerEntity)(Object)this;
@@ -21,6 +27,7 @@ public abstract class PlayerEntityMixin {
         ci.cancel();
     }
 
+    // Make the player sink in water if the armor they are wearing is too heavy
     @Inject(method = "travel", at = @At("HEAD"))
     private void sinkIfTooHeavy(Vec3d movementInput, CallbackInfo ci) {
         PlayerEntity player = (PlayerEntity)(Object)this;
@@ -39,6 +46,35 @@ public abstract class PlayerEntityMixin {
                 }
             }
         }
+    }
+
+    // Makes arrows only able to work in the hotbar or the offhand
+    @Inject(method = "getProjectileType", at = @At("HEAD"), cancellable = true)
+    private void restrictProjectileToHotbarAndOffhand(ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
+        if (!(stack.getItem() instanceof RangedWeaponItem rangedWeapon)) return;
+
+        Predicate<ItemStack> predicate = rangedWeapon.getHeldProjectiles();
+
+        PlayerEntity player = (PlayerEntity)(Object)this;
+
+        // Check offhand
+        ItemStack offhand = player.getOffHandStack();
+        if (predicate.test(offhand)) {
+            cir.setReturnValue(offhand);
+            return;
+        }
+
+        // Check hotbar (slots 0-8)
+        for (int i = 0; i < 9; i++) {
+            ItemStack stack1 = player.getInventory().getStack(i);
+            if (predicate.test(stack1)) {
+                cir.setReturnValue(stack1);
+                return;
+            }
+        }
+
+        // No arrow found
+        cir.setReturnValue(ItemStack.EMPTY);
     }
 
 }
