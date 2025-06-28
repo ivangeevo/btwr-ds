46df7c962a6f58303b9ae8054b858f1f734f061d
Ivan Geevo <dbowgaming@gmail.com>
2025-06-28 14:52:48 +0300

    bump changes

---
+++ src/main/java/org/ivangeevo/btwr_ds/mixin/GhastEntityMixin.java
@@ -0,0 +1,19 @@
+package org.ivangeevo.btwr_ds.mixin;
+
+import net.minecraft.entity.mob.GhastEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(GhastEntity.class)
+public abstract class GhastEntityMixin
+{
+
+    // Modifies the random that handles ghast spawning. This increases ghast spawn rate.
+    @ModifyArg(method = "canSpawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I"))
+    private static int modifyGhastSpawnRate(int bound)  {
+        return 5;
+    }
+
+
+}
