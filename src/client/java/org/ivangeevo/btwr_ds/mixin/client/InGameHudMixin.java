0ea36f0784f381fb6e5339ec97caf3a163ac4ef8
ivan <dbowgaming@gmail.com>
2025-07-08 00:09:37 +0300

    bump changes

--- src/client/java/org/ivangeevo/btwr_ds/mixin/client/InGameHudMixin.java
+++ src/client/java/org/ivangeevo/btwr_ds/mixin/client/InGameHudMixin.java
@@ -10,11 +10,23 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
-    @Shadow @Final private MinecraftClient client;
+
+
+    @ModifyArg(
+            method = "drawHeart",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud$HeartType;getTexture(ZZZ)Lnet/minecraft/util/Identifier;"),
+            index = 0 // index 0 is the first arg (hardcore check)
+    )
+    private boolean alwaysHardcore(boolean original) {
+        return true; // force hardcore heart style
+    }
+
 
     // Displays the saturation in the left top corner for debugging
     //@Inject(method = "render", at = @At("TAIL"))
