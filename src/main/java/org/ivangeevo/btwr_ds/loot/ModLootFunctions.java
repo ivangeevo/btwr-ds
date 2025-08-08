8a0e8bc95c63d2dcebd41a75fe11f1597ce573f4
ivan <dbowgaming@gmail.com>
2025-08-08 23:44:43 +0300

    bump changes

---
+++ src/main/java/org/ivangeevo/btwr_ds/loot/ModLootFunctions.java
@@ -0,0 +1,31 @@
+package org.ivangeevo.btwr_ds.loot;
+
+import net.minecraft.loot.function.LootFunctionType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.ivangeevo.btwr_ds.BTWRDSMod;
+
+public class ModLootFunctions {
+
+    public static final LootFunctionType<StatusEffectCountIncreaseLootFunction> STATUS_EFFECT_LOOT =
+            new LootFunctionType<>(StatusEffectCountIncreaseLootFunction.CODEC);
+
+    public static final LootFunctionType<StatusEffectApplyBonusLootFunction> STATUS_EFFECT_APPLY_BONUS =
+            new LootFunctionType<>(StatusEffectApplyBonusLootFunction.CODEC);
+
+
+    public static void register() {
+        Registry.register(
+                Registries.LOOT_FUNCTION_TYPE,
+                Identifier.of(BTWRDSMod.MOD_ID, "status_effect_loot"),
+                STATUS_EFFECT_LOOT
+        );
+
+        Registry.register(
+                Registries.LOOT_FUNCTION_TYPE,
+                Identifier.of(BTWRDSMod.MOD_ID, "status_effect_apply_bonus"),
+                STATUS_EFFECT_APPLY_BONUS
+        );
+    }
+}
