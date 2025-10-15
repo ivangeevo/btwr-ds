8b5bf0eef2e9babe010ecf6b297b6cb03dc3afc3
ivan <dbowgaming@gmail.com>
2025-10-16 01:20:19 +0300

    bump changes

--- src/client/java/org/ivangeevo/btwr_ds/BTWRDSModClient.java
+++ src/client/java/org/ivangeevo/btwr_ds/BTWRDSModClient.java
@@ -2,7 +2,12 @@ package org.ivangeevo.btwr_ds;
 
 import net.createmod.ponder.foundation.PonderIndex;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.impl.client.indigo.renderer.render.BlockRenderContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.ivangeevo.btwr_ds.config.BTWRDSSettings;
 import org.ivangeevo.btwr_ds.ponder.BTWRPonderPlugin;
 
@@ -31,7 +36,23 @@ public class BTWRDSModClient implements ClientModInitializer
 
 		//PonderIndex.addPlugin(new BTWRPonderPlugin());
 
+		/**
+		ItemTooltipCallback.EVENT.register((itemStack, tooltipContext, tooltipType, lines) -> {
+			if (isNetheriteToolOrArmor(itemStack.getItem())) {
+				// Insert after the first line (which is the item name)
+				int insertIndex = 1;
+				Text soulforged = Text.literal("[Soulforged]")
+						.formatted(Formatting.GOLD, Formatting.ITALIC);
+				lines.add(insertIndex, soulforged);
+			}
+		});
+		 **/
+	}
 
+	private static boolean isNetheriteToolOrArmor(Item item) {
+		// Example: check by item class or by tag
+		// Suppose you have a tag “mod:netherite_tools_armor”
+		return item.getDefaultStack().isOf(Items.NETHERITE_PICKAXE);
 	}
 
 
