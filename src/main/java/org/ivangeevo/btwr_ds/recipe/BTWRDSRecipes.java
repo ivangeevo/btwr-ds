61c8f3bd52e4921262b67a2e18f8f07f1ac8a0e0
ivan <dbowgaming@gmail.com>
2025-10-30 23:08:50 +0200

    bump changes

--- src/main/java/org/ivangeevo/btwr_ds/recipe/BTWRDSRecipes.java
+++ src/main/java/org/ivangeevo/btwr_ds/recipe/BTWRDSRecipes.java
@@ -7,24 +7,14 @@ import net.minecraft.recipe.RecipeType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import org.ivangeevo.btwr_ds.BTWRDSMod;
 
 public class BTWRDSRecipes {
 
     public static final DisabledRecipe.Serializer DISABLED_RECIPE_SERIALIZER = new DisabledRecipe.Serializer();
     public static final RecipeType<DisabledRecipe> DISABLED_RECIPE_TYPE = new RecipeType<>() {};
 
-    // Special recipe handlers for Material Beacons since it doesn't have its own recipe class
-    public static final MaterialBeaconsRecipe.Serializer MATERIAL_BEACONS_RECIPE_SERIALIZER = new MaterialBeaconsRecipe.Serializer();
-    public static final RecipeType<MaterialBeaconsRecipe> MATERIAL_BEACONS_RECIPE_TYPE = new RecipeType<>() {};
-
-
     public static void register() {
         registerRecipe("disabled", DISABLED_RECIPE_TYPE, DISABLED_RECIPE_SERIALIZER);
-        registerRecipe("material_beacons", MATERIAL_BEACONS_RECIPE_TYPE, MATERIAL_BEACONS_RECIPE_SERIALIZER);
-        Registry.register(Registries.RECIPE_TYPE, Identifier.of(MaterialBeacons.MOD_ID, "material_beacons"), MATERIAL_BEACONS_RECIPE_TYPE);
-        Registry.register(Registries.RECIPE_SERIALIZER, Identifier.of(MaterialBeacons.MOD_ID, "material_beacons"), MATERIAL_BEACONS_RECIPE_SERIALIZER);
-
     }
 
     private static void registerRecipe(String path, RecipeType<?> type, RecipeSerializer<?> serializer) {
