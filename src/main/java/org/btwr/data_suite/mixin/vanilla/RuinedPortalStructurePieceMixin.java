f33106c46962732546821e779abdb0d32cf754c5
ivan <dbowgaming@gmail.com>
2026-03-17 20:24:47 +0200

    some experimenting with mixin into ruined portal structure code

--- src/main/java/org/btwr/data_suite/mixin/vanilla/RuinedPortalStructurePieceMixin.java
+++ src/main/java/org/btwr/data_suite/mixin/vanilla/RuinedPortalStructurePieceMixin.java
@@ -1,39 +1,68 @@
 package org.btwr.data_suite.mixin.vanilla;
 
+import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.structure.RuinedPortalStructurePiece;
-import org.objectweb.asm.Opcodes;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.structure.*;
+import net.minecraft.structure.processor.*;
+import net.minecraft.structure.rule.AlwaysTrueRuleTest;
+import net.minecraft.structure.rule.RandomBlockMatchRuleTest;
+import net.minecraft.structure.rule.RuleTest;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(RuinedPortalStructurePiece.class)
-public abstract class RuinedPortalStructurePieceMixin {
+public abstract class RuinedPortalStructurePieceMixin extends SimpleStructurePiece {
 
-    @ModifyArg(
-            method = "createPlacementData(Lnet/minecraft/util/BlockMirror;Lnet/minecraft/util/BlockRotation;Lnet/minecraft/structure/RuinedPortalStructurePiece$VerticalPlacement;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/structure/RuinedPortalStructurePiece$Properties;)Lnet/minecraft/structure/StructurePlacementData;",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/structure/RuinedPortalStructurePiece;createReplacementRule(Lnet/minecraft/block/Block;FLnet/minecraft/block/Block;)Lnet/minecraft/structure/processor/StructureProcessorRule;", ordinal = 1), index = 0)
+
+    public RuinedPortalStructurePieceMixin(StructurePieceType type, int length, StructureTemplateManager structureTemplateManager, Identifier id, String template, StructurePlacementData placementData, BlockPos pos) {
+        super(type, length, structureTemplateManager, id, template, placementData, pos);
+    }
+
+    /**
+    @Inject(method = "createPlacementData(Lnet/minecraft/util/BlockMirror;Lnet/minecraft/util/BlockRotation;Lnet/minecraft/structure/RuinedPortalStructurePiece$VerticalPlacement;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/structure/RuinedPortalStructurePiece$Properties;)Lnet/minecraft/structure/StructurePlacementData;")
+    private static void injectedCreatePlacementData(BlockMirror mirror, BlockRotation rotation, RuinedPortalStructurePiece.VerticalPlacement verticalPlacement, BlockPos pos, RuinedPortalStructurePiece.Properties properties, CallbackInfoReturnable<StructurePlacementData> cir) {
+        BlockIgnoreStructureProcessor blockIgnoreStructureProcessor = properties.airPocket ? BlockIgnoreStructureProcessor.IGNORE_STRUCTURE_BLOCKS : BlockIgnoreStructureProcessor.IGNORE_AIR_AND_STRUCTURE_BLOCKS;
+        List<StructureProcessorRule> list = Lists.newArrayList();
+        list.add(createReplacementRule(Blocks.GOLD_BLOCK, 0.3F, Blocks.AIR));
+        list.add(createLavaReplacementRule(verticalPlacement, properties));
+        if (!properties.cold) {
+            list.add(createReplacementRule(Blocks.NETHERRACK, 0.07F, Blocks.MAGMA_BLOCK));
+        }
+
+        StructurePlacementData structurePlacementData = (new StructurePlacementData()).setRotation(rotation).setMirror(mirror).setPosition(pos).addProcessor(blockIgnoreStructureProcessor).addProcessor(new RuleStructureProcessor(list)).addProcessor(new BlockAgeStructureProcessor(properties.mossiness)).addProcessor(new ProtectedBlocksStructureProcessor(BlockTags.FEATURES_CANNOT_REPLACE)).addProcessor(new LavaSubmergedBlockStructureProcessor());
+        if (properties.replaceWithBlackstone) {
+            structurePlacementData.addProcessor(BlackstoneReplacementStructureProcessor.INSTANCE);
+        }
+
+        return structurePlacementData;
+
+    }
+    **/
+
+    //@ModifyArg(method = "createPlacementData(Lnet/minecraft/util/BlockMirror;Lnet/minecraft/util/BlockRotation;Lnet/minecraft/structure/RuinedPortalStructurePiece$VerticalPlacement;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/structure/RuinedPortalStructurePiece$Properties;)Lnet/minecraft/structure/StructurePlacementData;", at = @At(value = "INVOKE", target = "Lnet/minecraft/structure/RuinedPortalStructurePiece;createReplacementRule(Lnet/minecraft/block/Block;FLnet/minecraft/block/Block;)Lnet/minecraft/structure/processor/StructureProcessorRule;", ordinal = 1), index = 0)
     private static Block replaceNetherrackPlacementData(Block old) {
         return Blocks.NETHER_BRICKS;
     }
 
-    @ModifyArg(
-            method = "createLavaReplacementRule",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/structure/RuinedPortalStructurePiece;createReplacementRule(Lnet/minecraft/block/Block;Lnet/minecraft/block/Block;)Lnet/minecraft/structure/processor/StructureProcessorRule;", ordinal = 1), index = 1)
+    //@ModifyArg(method = "createLavaReplacementRule", at = @At(value = "INVOKE", target = "Lnet/minecraft/structure/RuinedPortalStructurePiece;createReplacementRule(Lnet/minecraft/block/Block;Lnet/minecraft/block/Block;)Lnet/minecraft/structure/processor/StructureProcessorRule;", ordinal = 1), index = 1)
     private static Block replaceNetherrackLavaReplacementRule(Block old) {
         return Blocks.NETHER_BRICKS;
     }
 
-    @Redirect(
-            method = "updateNetherracksInBound",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"
-            )
-    )
+    //@Redirect(method = "updateNetherracksInBound", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"))
     private boolean redirectIsOf(BlockState instance, Block block) {
         // treat original netherrack checks as nether bricks
         if (block == Blocks.NETHERRACK) {
@@ -42,18 +71,12 @@ public abstract class RuinedPortalStructurePieceMixin {
         return instance.isOf(block);
     }
 
-    @Redirect(
-            method = "generateOvergrownLeaves",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;NETHERRACK:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC)
-    )
+    //@Redirect(method = "generateOvergrownLeaves", at = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;NETHERRACK:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC))
     private Block replaceNetherrackOvergrownLeaves() {
         return Blocks.NETHER_BRICKS;
     }
 
-    @Redirect(
-            method = "placeNetherrackBottom",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;NETHERRACK:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC)
-    )
+    //@Redirect(method = "placeNetherrackBottom", at = @At(value = "FIELD", target = "Lnet/minecraft/block/Blocks;NETHERRACK:Lnet/minecraft/block/Block;", opcode = Opcodes.GETSTATIC))
     private Block replaceNetherrackBottom() {
         return Blocks.NETHER_BRICKS;
     }
