29a9c3ad23923bac644701a1c1ff52e2fd73d7b7
ivan <dbowgaming@gmail.com>
2026-03-17 20:25:31 +0200

    replaced netherrack in the overworld with blackstone via fabric server chunk events

--- src/main/java/org/btwr/data_suite/util/WorldGenBlockReplacements.java
+++ src/main/java/org/btwr/data_suite/util/WorldGenBlockReplacements.java
@@ -1,11 +1,14 @@
 package org.btwr.data_suite.util;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerChunkEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.registry.Registries;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.DyeColor;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.dimension.DimensionTypes;
 import org.btwr.self_sustainable.block.ModBlocks;
 import org.btwr.shared_library.util.utils.IdUtils;
 
@@ -19,10 +22,6 @@ public class WorldGenBlockReplacements {
     // so it should contain only blocks that the player shouldn't have access to right away.
     // Any other replacements of blocks for specific things (like structures only) should be handled differently.
     public static void register() {
-
-        // For ruined portals
-        BlockReplacementRegistry.registerReplacement(Blocks.NETHERRACK, Blocks.NETHER_BRICKS);
-
         // remove beds
         for (DyeColor color : DyeColor.values()) {
             Block bedBlock = Registries.BLOCK.get(IdUtils.ofMC(color.asString() + "_bed"));
@@ -43,7 +42,8 @@ public class WorldGenBlockReplacements {
         replaceWithCopyFacing(Blocks.TORCH, ModBlocks.CRUDE_TORCH_BURNED_OUT, Properties.HORIZONTAL_FACING);
         replaceWithCopyFacing(Blocks.FURNACE, ModBlocks.OVEN_BRICK, Properties.HORIZONTAL_FACING);
 
-        //ServerChunkGenerateEvents.createChunkReplaceEventGlobally(Blocks.GRASS_BLOCK, Blocks.RED_STAINED_GLASS);
+        // With fabric's replace events
+        registerWithEvents();
     }
 
     private static void replaceWithCopyFacing(Block target, Block replacement, DirectionProperty directionProperty) {
@@ -68,6 +68,28 @@ public class WorldGenBlockReplacements {
     /** Simply replaces a block with air **/
     private static void removeBlock(Block block) {
         BlockReplacementRegistry.registerReplacement(block, Blocks.AIR);
+    }
+
+
+    private static void registerWithEvents() {
+        // Replace netherrack in the overworld with nether bricks to prevent early nethercoal
+        ServerChunkEvents.CHUNK_GENERATE.register((world, chunk) -> {
+            BlockPos.Mutable pos = new BlockPos.Mutable();
+
+            if (!world.getDimensionEntry().matchesId(DimensionTypes.OVERWORLD_ID)) return;
+
+            for(int y = -64; y <= 319; ++y) {
+                for(int x = 0; x < 16; ++x) {
+                    for(int z = 0; z < 16; ++z) {
+                        pos.set(x + chunk.getPos().getStartX(), y, z + chunk.getPos().getStartZ());
+                        if (chunk.getBlockState(pos).isOf(Blocks.NETHERRACK)) {
+                            chunk.setBlockState(pos, Blocks.BLACKSTONE.getDefaultState(), false);
+                        }
+                    }
+                }
+            }
+
+        });
     }
 
 }
