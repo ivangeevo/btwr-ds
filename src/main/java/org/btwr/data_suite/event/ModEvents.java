170aa8b8589ec7d2df40c3129f8cb2cb145772b8
ivan <dbowgaming@gmail.com>
2026-04-04 17:44:14 +0300

    changed default wolf health to 20

--- src/main/java/org/btwr/data_suite/event/ModEvents.java
+++ src/main/java/org/btwr/data_suite/event/ModEvents.java
@@ -1,5 +1,9 @@
 package org.btwr.data_suite.event;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.passive.WolfEntity;
 import org.btwr.data_suite.event.events.FoodComponentModifierEvents;
 import org.btwr.data_suite.event.events.ItemCountModificationEvents;
 import org.btwr.data_suite.event.events.ModLootTableEvents;
@@ -11,5 +15,16 @@ public class ModEvents {
         ItemCountModificationEvents.register();
         ModLootTableEvents.register();
         //ModTillingLootTableEvents.register();
+
+        // Modify Wolf health
+        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
+            if (entity instanceof WolfEntity wolf) {
+                EntityAttributeInstance health = wolf.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+                if (health != null) {
+                    health.setBaseValue(20.0);
+                    wolf.setHealth(20.0f); // Also update current HP
+                }
+            }
+        });
     }
 }
