a839ca72e18af6e65d2bec1c9f198969c697d87c
Ivan Geevo <4586116+ivangeevo@users.noreply.github.com>
2026-09-12 17:48:41 +0300

    + Fixed modern axes item tag containing the stone axe which was added by Sturdy Trees for its own behavior which we don't need in the modpack + Removed scrambled eggs from Self-Sustainable as valid cooked egg foods from the BTWRConvetionalTags "COOKED_EGG_FOODS" tag. This was made in order to have a valid item to use when crafting Ham and Eggs when BWT's fried/cooked eggs aren't present, but we don't want this in the modpack + ItemTagProvider reorganize

--- src/main/java/org/btwr/data_suite/datagen/DS_ItemTagProvider.java
+++ src/main/java/org/btwr/data_suite/datagen/DS_ItemTagProvider.java
@@ -1,8 +1,10 @@
 package org.btwr.data_suite.datagen;
 
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 import org.btwr.animageddon.tag.ModTags;
-import org.btwr.core.item.BTWR_Items;
 import org.btwr.core.tag.BTWRTags;
+import org.btwr.self_sustainable.item.ModItems;
 import org.btwr.shared_library.api.tag.BTWRConventionalTags;
 import com.bwt.items.BwtItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
@@ -14,6 +16,8 @@ import net.minecraft.registry.tag.ItemTags;
 import java.util.concurrent.CompletableFuture;
 
 import static org.btwr.animageddon.item.ModItems.BURNED_MEAT;
+import static org.btwr.core.item.ModItems.*;
+import static org.btwr.self_sustainable.item.ModItems.*;
 import static org.btwr.vegehenna.item.ModItems.*;
 
 public class DS_ItemTagProvider extends FabricTagProvider.ItemTagProvider {
@@ -23,32 +27,58 @@ public class DS_ItemTagProvider extends FabricTagProvider.ItemTagProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
-        // Vanilla tags
-        getOrCreateTagBuilder(ItemTags.WOLF_FOOD)
-                .add(BTWR_Items.SANDWICH)
-                .add(BTWR_Items.HAM_AND_EGGS)
-                .add(BTWR_Items.STEAK_AND_POTATOES)
-                .add(BTWR_Items.RAW_KEBAB)
-                .add(BTWR_Items.COOKED_KEBAB)
-                .add(BTWR_Items.STEAK_DINNER)
-                .add(BTWR_Items.PORK_DINNER)
-                .add(BTWR_Items.WOLF_DINNER)
-                .add(BTWR_Items.BEAST_LIVER_RAW)
-                .add(BTWR_Items.BEAST_LIVER_COOKED)
-                .add(BURNED_MEAT);
+        addToVanillaTags();
+        addToModTags();
+        addToOtherModTags();
+        addToConventionalTags();
+        addToBTWRConventionalTags();
+    }
 
+    private void addToVanillaTags() {
+        getOrCreateTagBuilder(ItemTags.WOLF_FOOD)
+                .add(SANDWICH)
+                .add(HAM_AND_EGGS)
+                .add(STEAK_AND_POTATOES)
+                .add(RAW_KEBAB)
+                .add(COOKED_KEBAB)
+                .add(STEAK_DINNER)
+                .add(PORK_DINNER)
+                .add(WOLF_DINNER)
+                .add(BEAST_LIVER_RAW)
+                .add(BEAST_LIVER_COOKED)
+                .add(BURNED_MEAT);
+    }
+
+    private void addToModTags() {
+        getOrCreateTagBuilder(ModTags.Items.PIG_BREEDING_ITEMS)
+                .add(CHOCOLATE);
+    }
+
+    private void addToOtherModTags() {
+        getOrCreateTagBuilder(BTWRTags.Items.TANNED_LEATHERS)
+                .add(BwtItems.tannedLeatherItem);
+    }
+
+    private void addToConventionalTags() {
+        getOrCreateTagBuilder(ConventionalItemTags.STRINGS)
+                .add(BwtItems.hempFiberItem);
+
+        // Replacing because Self-Sustainable adds scrambled eggs for compat if BWT isn't present, and we don't want that in the modpack
+        getOrCreateTagBuilder(BTWRConventionalTags.Items.COOKED_EGG_FOODS)
+                .setReplace(true)
+                .addOptional(Identifier.of("bwt", "fried_egg"))
+                .addOptional(Identifier.of("bwt", "poached_egg"));
+    }
+
+    private void addToBTWRConventionalTags() {
         getOrCreateTagBuilder(BTWRConventionalTags.Items.CHICKEN_TEMPT_ITEMS)
                 .add(BwtItems.hempSeedsItem)
                 .add(CARROT_SEEDS);
 
-        // Conventional tags
-        getOrCreateTagBuilder(ConventionalItemTags.STRINGS)
-                .add(BwtItems.hempFiberItem);
-
-        // BTWR Conventional tags
-        getOrCreateTagBuilder(BTWRConventionalTags.Items.COOKED_EGG_FOODS)
-                .add(BwtItems.friedEggItem)
-                .add(BwtItems.poachedEggItem);
+        getOrCreateTagBuilder(BTWRConventionalTags.Items.MODERN_AXES)
+                .setReplace(true)
+                .add(Items.IRON_AXE)
+                .add(Items.DIAMOND_AXE);
 
         getOrCreateTagBuilder(BTWRConventionalTags.Items.DO_KNOCKBACK_ITEMS)
                 .add(BwtItems.netheriteMattockItem)
@@ -60,11 +90,10 @@ public class DS_ItemTagProvider extends FabricTagProvider.ItemTagProvider {
         getOrCreateTagBuilder(BTWRConventionalTags.Items.ON_CRAFT_SHEARS_CUT_SOUND)
                 .add(BwtItems.strapItem);
 
-        // Misc tags
-        getOrCreateTagBuilder(BTWRTags.Items.TANNED_LEATHERS)
-                .add(BwtItems.tannedLeatherItem);
-
-        getOrCreateTagBuilder(ModTags.Items.PIG_BREEDING_ITEMS)
-                .add(CHOCOLATE);
+        // Replace the cooked egg foods because "Self Sustainable" adds scrambled eggs for compat, but we don't need that in BTWR
+        getOrCreateTagBuilder(BTWRConventionalTags.Items.COOKED_EGG_FOODS)
+                .setReplace(true)
+                .addOptional(Identifier.of("bwt", "fried_egg"))
+                .addOptional(Identifier.of("bwt", "poached_egg"));
     }
 }
